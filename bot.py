08a28e24e3cc34a9840225d872d6826b521de0e7
Equarq1 <gaifullineduard@mail.ru>
2025-09-23 23:36:04 +0300

    last - исправленная версия

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+#mgm
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
